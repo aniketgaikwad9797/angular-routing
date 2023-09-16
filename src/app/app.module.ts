317b5dc499614d8172bb1dafd7525354726d9d8b
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-09-16 18:55:21 +0530

    Fetching route parameters reactiely

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ const applicationRoutes: Routes = [
     component: HomeComponent,
   },
   { path: 'users', component: UsersComponent },
-  { path: 'users/:userId', component: UserComponent },
+  { path: 'users/:userId/:userName', component: UserComponent },
   { path: 'servers', component: ServersComponent },
 ];
 
